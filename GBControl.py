82a8312132b9eaf8121a53702b7c8d444c38b6a6
LnksGit <lnkstls@gmail.com>
2020-03-28 14:14:07 +0800

    1

--- GBControl.py
+++ GBControl.py
@@ -1,10 +1,10 @@
+#! /usr/bin/env python3
 
-#!/usr/bin/python3
 import os
 limit_total = 0  # limit_total 上传+下载的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
 limit_in = 0  # limit_in 下载的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
 limit_out = 0  # limit_out 上传的流量限制，单位GB，如果不限制就是0，如果限制1T就是1024
-
+limit_rema = 1 #unit GB 剩余流量限制
 
 NET_IN = 0
 NET_OUT = 0
@@ -16,12 +16,22 @@ for line in vnstat:
         NET_IN = int(mdata[3])/1024
         NET_OUT = int(mdata[4])/1024
 
-killssr = "systemctl stop docker || echo '已关闭docker'"
-if (limit_total != 0 and (NET_IN+NET_OUT) >= limit_total):
+killssr = "halt"
+if (limit_total != 0 and (NET_IN+NET_OUT) >= (limit_total-limit_rema)):
     os.system(killssr)
-elif (limit_in != 0 and NET_IN >= limit_in):
+elif (limit_in != 0 and NET_IN >= (limit_in-limit_rema)):
     os.system(killssr)
-elif (limit_out != 0 and NET_OUT >= limit_out):
+elif (limit_out != 0 and NET_OUT >= (limit_out-limit_rema)):
     os.system(killssr)
 
+
 print("NET_IN=", NET_IN, "---NET_OUT=", NET_OUT, "---total=", NET_IN+NET_OUT)
+print("rema=", limit_rema, end="，")
+if (limit_total != 0):
+    print("total=", limit_total)
+elif (limit_in != 0):
+    print("in=", limit_in)
+elif (limit_out !=0): 
+    print("out=", limit_out)
+else:
+    print("limit=null")
