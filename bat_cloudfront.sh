c5dcdfc6f9cfd81528af8b3b0bf21df6e74ae7c5
Lnkstls <41938676+Lnkstls@users.noreply.github.com>
2020-11-03 03:10:41 +0800

    Update bat_cloudfront.sh

--- bat_cloudfront.sh
+++ bat_cloudfront.sh
@@ -5,8 +5,12 @@ export PATH
 if (( $#!=0 )) ;then
 if [[ -e $1 ]] ;then
 while read line ;do
-python3 cloudfront.py $line $2 &&
-	echo "${line}完成 !"
+python3 cloudfront.py $line $2
+if (( $?=0 )) ;then
+echo "${line}完成 !"
+else
+break
+fi
 done <$1
 else
 echo "文件不存在 !" && exit 1
