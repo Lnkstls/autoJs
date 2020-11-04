83dc2455604e5a9a30513c7e35680bea9bb82e81
Lnkstls <lnkstls@gmail.com>
2020-11-04 21:11:54 +0800

    update

---
+++ dnspod.sh
@@ -0,0 +1,83 @@
+#Dnspod DDNS with BashShell
+#Github:https://github.com/kkkgo/dnspod-ddns-with-bashshell
+#More: https://03k.org/dnspod-ddns-with-bashshell.html
+#CONF START
+
+while getopts ":i:t:d:h:l:" opt; do
+  case "$opt" in
+  i)
+    API_ID=${OPTARG:-12345}
+    ;;
+  t)
+    API_Token=${OPTARG:-abcdefghijklmnopq2333333}
+    ;;
+  d)
+    domain=${OPTARG:-example.com}
+    ;;
+  h)
+    host=${OPTARG:-home}
+    ;;
+  l)
+    ttl=${OPTARG:-600}
+    ;;
+  *)
+    echo "
+      -i API_ID
+      -t API_Token
+      -d domain
+      -h host
+      -l ttl default 600
+      "
+    ;;
+  esac
+done
+
+# echo "${API_ID}、${API_Token}、${domain}、${host}、${ttl}"
+
+CHECKURL="http://ip.03k.org"
+#OUT="pppoe"
+#CONF END
+. /etc/profile
+date
+if (echo $CHECKURL | grep -q "://"); then
+  IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+  URLIP=$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s $CHECKURL | grep -Eo "$IPREX" | tail -n1)
+  if (echo $URLIP | grep -qEvo "$IPREX"); then
+    URLIP="Get $DOMAIN URLIP Failed."
+  fi
+  echo "[URL IP]:$URLIP"
+  dnscmd="nslookup"
+  type nslookup >/dev/null 2>&1 || dnscmd="ping -c1"
+  DNSTEST=$($dnscmd $host.$domain)
+  if [ "$?" != 0 ] && [ "$dnscmd" = "nslookup" ] || (echo $DNSTEST | grep -qEvo "$IPREX"); then
+    DNSIP="Get $host.$domain DNS Failed."
+  else
+    DNSIP=$(echo $DNSTEST | grep -Eo "$IPREX" | tail -n1)
+  fi
+  echo "[DNS IP]:$DNSIP"
+  if [ "$DNSIP" = "$URLIP" ]; then
+    echo "IP SAME IN DNS,SKIP UPDATE."
+    exit
+  fi
+fi
+token="login_token=${API_ID},${API_Token}&format=json&lang=en&error_on_empty=yes&domain=${domain}&sub_domain=${host}"
+Record="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.List -d "${token}")"
+iferr="$(echo ${Record#*code} | cut -d'"' -f3)"
+if [ "$iferr" = "1" ]; then
+  record_ip=$(echo ${Record#*value} | cut -d'"' -f3)
+  echo "[API IP]:$record_ip"
+  if [ "$record_ip" = "$URLIP" ]; then
+    echo "IP SAME IN API,SKIP UPDATE."
+    exit 1
+  fi
+  record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"} | cut -d'"' -f2)
+  record_line_id=$(echo ${Record#*line_id} | cut -d'"' -f3)
+  echo Start DDNS update...
+  ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Modify -d "${token}&record_type=A&record_id=${record_id}&record_line_id=${record_line_id}&ttl=${ttl}&value=${URLIP}")"
+  ddns_result="$(echo ${ddns#*message\"} | cut -d'"' -f2)"
+  echo -n "DDNS upadte result:$ddns_result "
+  echo $ddns | grep -Eo "$IPREX" | tail -n1
+else
+  echo -n Get $host.$domain error :
+  echo $(echo ${Record#*message\"}) | cut -d'"' -f2
+fi
---
+++ dnspod_line.sh
@@ -0,0 +1,55 @@
+#Dnspod DDNS with BashShell
+#Github:https://github.com/kkkgo/dnspod-ddns-with-bashshell
+#More: https://03k.org/dnspod-ddns-with-bashshell.html
+#CONF START
+API_ID=12345
+API_Token=abcdefghijklmnopq2333333
+domain=example.com
+host=home
+#OUT="pppoe"
+DEV="eth0"
+#CONF END
+. /etc/profile
+date
+IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+ipcmd="ip addr show"
+type ip >/dev/null 2>&1 || ipcmd="ifconfig"
+DEVIP=$($ipcmd $DEV | grep -Eo "$IPREX" | head -n1)
+if (echo $DEVIP | grep -qEvo "$IPREX"); then
+    DEVIP="Get $DOMAIN DEVIP Failed."
+fi
+echo "[DEV IP]:$DEVIP"
+dnscmd="nslookup"
+type nslookup >/dev/null 2>&1 || dnscmd="ping -c1"
+DNSTEST=$($dnscmd $host.$domain)
+if [ "$?" != 0 ] && [ "$dnscmd" == "nslookup" ] || (echo $DNSTEST | grep -qEvo "$IPREX"); then
+    DNSIP="Get $host.$domain DNS Failed."
+else
+    DNSIP=$(echo $DNSTEST | grep -Eo "$IPREX" | tail -n1)
+fi
+echo "[DNS IP]:$DNSIP"
+if [ "$DNSIP" = "$DEVIP" ]; then
+    echo "IP SAME IN DNS,SKIP UPDATE."
+    exit
+fi
+token="login_token=${API_ID},${API_Token}&format=json&lang=en&error_on_empty=yes&domain=${domain}&sub_domain=${host}"
+Record="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.List -d "${token}")"
+iferr="$(echo ${Record#*code} | cut -d'"' -f3)"
+if [ "$iferr" = "1" ]; then
+    record_ip=$(echo ${Record#*value} | cut -d'"' -f3)
+    echo "[API IP]:$record_ip"
+    if [ "$record_ip" == "$DEVIP" ]; then
+        echo "IP SAME IN API,SKIP UPDATE."
+        exit
+    fi
+    record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"} | cut -d'"' -f2)
+    record_line_id=$(echo ${Record#*line_id} | cut -d'"' -f3)
+    echo Start DDNS update...
+    ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}&value=$DEVIP")"
+    ddns_result="$(echo ${ddns#*message\"} | cut -d'"' -f2)"
+    echo -n "DDNS upadte result:$ddns_result "
+    echo $ddns | grep -Eo "$IPREX" | tail -n1
+else
+    echo -n Get $host.$domain error :
+    echo $(echo ${Record#*message\"}) | cut -d'"' -f2
+fi
