3da40784b757f049d5e03d71b60226b73343fb5b
Lnkstls <lnkstls@gmail.com>
2020-12-11 02:07:15 +0800

    update

--- poseidon.sh
+++ poseidon.sh
@@ -49,22 +49,22 @@ fi
 Release=$(cat /etc/os-release | grep "VERSION_ID" | awk -F '=' '{print $2}' | sed "s/\"//g")
 
 update_sh() {
-  local github="https://raw.githubusercontent.com/Lnkstls/autoJs/master/"
+  uname="poseidon.sh"
   echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
-  local sh_new_ver=$(wget -qO- "${github}/poseidon.sh" | grep 'sh_ver="' | awk -F "=" '{print $NF}' | sed 's/\"//g' | head -1)
-  [[ -z ${sh_new_ver} ]] && echo -e "${Error} 检测最新版本失败 !" && sleep 3s && start_menu
+  local sh_new_ver=$(wget -qO- "${lnkstls_link}/${uname}" | grep 'sh_ver="' | awk -F "=" '{print $NF}' | sed 's/\"//g' | head -1)
+  [[ -z ${sh_new_ver} ]] && echo -e "${error}检测最新版本失败 !" && sleep 3s && start_menu
   if [[ ${sh_new_ver} != ${sh_ver} ]]; then
     echo -e "${info}发现新版本[ ${sh_new_ver} ]，是否更新？[Y/n]"
     read -p "(默认: y): " yn
     [[ -z "${yn}" ]] && yn="y"
     if [[ ${yn} == [Yy] ]]; then
-      wget -O poseidon.sh "${github}/poseidon.sh" && chmod +x poseidon.sh
-      echo -e "${info}脚本已更新为最新版本[ ${sh_new_ver} ]!" && exit 0
+      wget "${lnkstls_link}/${uname}.sh" && chmod +x ${uname}.sh
+      echo -e "${info}脚本已更新为最新版本[ ${sh_new_ver} ] !" && exit 0
     else
       echo && echo "${info}已取消..." && echo
     fi
   else
-    echo -e "${info}当前已是最新版本[ ${sh_new_ver} ]!"
+    echo -e "${info}当前已是最新版本[ ${sh_new_ver} ] !"
     sleep 5s
     start_menu
   fi
